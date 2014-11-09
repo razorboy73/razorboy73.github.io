285eb31b1bab0688cc3f6bd22407d7b8f025ce3e
razorboy73 <josh.kerbel@femme-o-nomics.com>
2014-11-09 16:45:27 -0500

    added case studies and associted sendmail files

---
+++ sendmail-succession.php
@@ -0,0 +1,17 @@
+<?php
+  $name = $_REQUEST['name'];
+  $company = $_REQUEST['company'];
+  $email = $_REQUEST['email'] ;
+  $phone = $_REQUEST['phone'];
+
+  $emess = "Name: ".$name."\n";
+  $emess.= "Company: ".$company."\n";
+  $emess.= "Email : ".$email."\n";
+  $emess.= "Phone number: ".$phone."\n";
+
+
+  mail( "dave@rallyyourgoals.com", "Case Study for Insurance Companies - What Skills Are Walking Out the Door
+",
+    $emess, "From: $email" );
+  header( "Location: /casestudyrequestthankyou.html" );
+?>
