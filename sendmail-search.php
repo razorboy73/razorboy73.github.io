dfb46962690562657faebb40ea2caa3a768ee061
razorboy73 <josh.kerbel@femme-o-nomics.com>
2014-11-09 20:43:18 -0500

    adding landing pages

--- sendmail-search.php
+++ sendmail-search.php
@@ -10,7 +10,7 @@
   $emess.= "Phone number: ".$phone."\n";
 
 
-  mail( "dave@rallyyourgoals.com", "Case Study for Large Enterprises - Real Estate Case Study",
+  mail( "dave@rallyyourgoals.com", "Human Capital Search Case Study for Large Enterprises - Real Estate Case Study",
     $emess, "From: $email" );
-  header( "Location: /casestudyrequestthankyou.html" );
+  header( "Location: /casestudy-download-search.html" );
 ?>
