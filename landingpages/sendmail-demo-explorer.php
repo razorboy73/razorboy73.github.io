7c9441f8d20cf4eecd1cea89081e6a8fb6b4ccab
razorboy73 <josh.kerbel@femme-o-nomics.com>
2014-12-04 22:53:27 -0500

    added php mailer for landing page

---
+++ landingpages/sendmail-demo-explorer.php
@@ -0,0 +1,17 @@
+<?php
+  $name = $_REQUEST['name'];
+  $company = $_REQUEST['company'];
+  $email = $_REQUEST['email'] ;
+  $phone = $_REQUEST['phone'];
+
+  $emess = "Name: ".$name."\n";
+  $emess.= "Company: ".$company."\n";
+  $emess.= "Email : ".$email."\n";
+  $emess.= "Phone number: ".$phone."\n";
+
+
+  mail( "dave@rallyyourgoals.com", "Demo request r/ally Explorer Case Study for Professional Staffing & Recruiting
+Staffing and Recruiting Case Study",
+    $emess, "From: $email" );
+  header( "Location: /demo-download-explorer.html" );
+?>
