bbd8ac6d998e2fa2420da9f3ed523059aec7519e
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-01-29 15:53:59 -0500

    marketing-14

--- landingpages/sendmail-demo-explorer.php
+++ landingpages/sendmail-demo-explorer.php
@@ -3,15 +3,18 @@
   $company = $_REQUEST['company'];
   $email = $_REQUEST['email'] ;
   $phone = $_REQUEST['phone'];
+  $job = $_REQUEST['job'];
+  $demo_wanted = $_REQUEST['demo_wanted'];
 
   $emess = "Name: ".$name."\n";
   $emess.= "Company: ".$company."\n";
   $emess.= "Email : ".$email."\n";
   $emess.= "Phone number: ".$phone."\n";
+  $emess.= "job: ".$job."\n";
+  $emess.= "demo wanted: ".$demo_wanted."\n";
 
 
-  mail( "dave@rallyyourgoals.com", "Demo request r/ally Explorer Case Study for Professional Staffing & Recruiting
-Staffing and Recruiting Case Study",
+  mail( "dave@rallyyourgoals.com", "Demo request",
     $emess, "From: $email" );
   header( "Location: /landingpages/demo-download-explorer.html" );
 ?>
