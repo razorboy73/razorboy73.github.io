380338aa1c81024ee3b358e64ededf0ccd693f24
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-02-17 17:55:13 -0500

    adding semiconductor download pages

---
+++ sendmail-search-semiconductor.php
@@ -0,0 +1,16 @@
+<?php
+  $name = $_REQUEST['name'];
+  $company = $_REQUEST['company'];
+  $email = $_REQUEST['email'] ;
+  $phone = $_REQUEST['phone'];
+
+  $emess = "Name: ".$name."\n";
+  $emess.= "Company: ".$company."\n";
+  $emess.= "Email : ".$email."\n";
+  $emess.= "Phone number: ".$phone."\n";
+
+
+  mail( "josh@rallyyourgoals.com", "Human Capital Search Case Study - semiconductor",
+    $emess, "From: $email" );
+  header( "Location: /casestudy-download-search-semiconductor.html" );
+?>
